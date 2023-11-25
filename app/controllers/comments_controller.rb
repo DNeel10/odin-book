65e7da7097d83311dfd337f72610d46b4600a7bb
Daniel Neel <dneel10@gmail.com>
2023-11-25 14:45:36 -0500

    working through comments build through users and posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
+
+  resources :posts
+  resources :comments
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :posts
 end

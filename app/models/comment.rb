1df0df246dd72c99fbad041a9a668b939991457f
Daniel Neel <dneel10@gmail.com>
2024-05-20 20:12:48 -0400

    add validation on body for posts and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user, counter_cache: true
   has_many :likes, as: :likeable, dependent: :destroy
 
+  # Polymorphic association to a Commentable object type
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
+
+  validates :body, presence: true
 
   def liked_by?(user)
     likes.exists?(user: user)
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
 
+  validates :body, presence: true
+
   scope :by_followed_user, -> (user) { 
     followed_user_ids = user.followed_users.pluck(:id)
     where(user_id: followed_user_ids).or(where(user_id: user.id))
